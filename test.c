0691cd5bbaa884f35d13b7760581fa3e8ee4e986
Shvayne <122893327+Shvayne@users.noreply.github.com>
2023-02-25 09:29:34 +0100

    print text

--- test.c
+++ test.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define incost 75
+#define mincharge 120
+/*a program that reads values for hours worked and cost of parts 
+and print the charge.
+puesdocode- prompt for cost of parts
+prompt for hours worked
+if no work needs to be done, charge = 75
+if theres work , theres no charge for inspecting*/
 int main(void)
 {
-    printf("hello world\n")
+    int c;
+    double a, b;
+    printf("how much parts?\n");
+    scanf("%lf", &a);
+    printf("how many hours?\n");
+    scanf("%lf", &b);
+    if (b <= 0)
+    {
+        c = mincharge;
+        printf("your charge is $%d\n", c);
+        else if (b > 0)
+        c = incost + a;
+
+    }
+return;
 }
